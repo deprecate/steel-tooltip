73f1d141ce2a1e51db0799453c1b58847e69fb95
Maira Bello <maira.araujo@liferay.com>
2015-12-01 09:13:51 -0300

    Fixes karma coverage file

--- karma-coverage.conf.js
+++ karma-coverage.conf.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var metalKarmaConfig = require('metal-karma-config');
+var metalKarmaConfig = require('metal-karma-config/coverage');
 
 module.exports = function (config) {
 	metalKarmaConfig(config);
